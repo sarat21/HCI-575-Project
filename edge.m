380d82f197da317543cfbbeb13285d9b46690e68
sarat21 <sarat@iastate.edu>
2016-08-03 09:48:25 -0400

    Create edge.m

---
+++ edge.m
@@ -0,0 +1,21 @@
+% i wish matlab had structs
+
+classdef Edge
+    properties
+        pos;
+    end
+    
+    methods
+        function obj = Edge(x, y)
+            obj.pos = [y x];
+        end
+        
+        function num = i(obj)
+            num = obj.pos(2);
+        end
+        
+        function num = j(obj)
+            num = obj.pos(1);
+        end
+    end
+end

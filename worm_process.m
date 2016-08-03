881e7ea70b2b8ec0f9dc203f8ca06f73cc0916ac
sarat21 <sarat@iastate.edu>
2016-08-03 09:55:02 -0400

    Create worm_process.m

---
+++ worm_process.m
@@ -0,0 +1,7 @@
+function target = worm_process(input_image)
+    target = imerode(input_image, ones(2));
+    target = imdilate(target, strel('disk',30));
+    target = imclose(target,strel('disk',30));
+    target = imerode(target, strel('disk',30));
+    target = target > 7;
+end
